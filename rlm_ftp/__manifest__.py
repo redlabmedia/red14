5cbe588edfc6738b3e2dda67af956841a1d8e64b
sugapablo <russ@sugapablo.com>
2021-05-05 10:29:19 -0400

    0.2

--- rlm_ftp/__manifest__.py
+++ rlm_ftp/__manifest__.py
@@ -17,14 +17,14 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
